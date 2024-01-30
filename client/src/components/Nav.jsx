f1732c5390569487bb0e3f5023972c2c3d3c7d38
Arnav Lahane <119531646+Arrnv@users.noreply.github.com>
2024-01-31 01:05:36 +0530

    Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,8 +8,8 @@ const Nav = () => {
         <nav>
             <Link to="/"><img src="../src/assets/Predict Icon.png" alt="heart"  /></Link>
             <Link to="/report"><img src="../src/assets/Monthly Report Icon.png" alt="Monthly Report"  /></Link>
-            <Link><img src="../src/assets/About Icon.png" alt="About Icon"  /></Link>
-            <Link><img src="../src/assets/Profile Icon.png" alt="Profile Icon"  /></Link>
+            <Link to="/About"><img src="../src/assets/About Icon.png" alt="About Icon"  /></Link>
+            <Link to="/profile" ><img src="../src/assets/Profile Icon.png" alt="Profile Icon"  /></Link>
         </nav>
         <Outlet/>
     </>
