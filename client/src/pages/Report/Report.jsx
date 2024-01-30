b8e5ec5345f85a343ade508a8794c9e1b6ffc82b
Arnav Lahane <119531646+Arrnv@users.noreply.github.com>
2024-01-31 01:11:45 +0530

    navbar and pages

--- client/src/pages/Report/Report.jsx
+++ client/src/pages/Report/Report.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Report.css"
-import Nav from "../components/Nav"
+import Nav from "../../components/Nav"
 const Report = () => {
   return (
     <div className='true'>
